7dcf81b42201431c009cfadcad5f38aeed8a8bb7
Emanuel Joness <itzemanuelj@gmail.com>
2022-09-01 00:25:59 -0400

    mvp commit for if statements

---
+++ if statements.py
@@ -0,0 +1,81 @@
+# Rewrite this line so it's valid:
+# if species = "cat":
+from re import X
+
+
+species = ""
+
+if species == "cat":
+    print(species)
+
+# Rewrite this line so it's valid:
+# if species == "cat"
+
+species = ""
+
+if species == "cat":
+    print(species)
+
+# Code the first line of an if statement that tests whether x has the same value as y
+x = 1
+y = 2
+
+if x == y:
+    print(x)
+
+
+# Code the first line of an if statement that tests whether one variable is the same as another. Make up the variable names.
+a = 1
+b = 2
+
+if a == b:
+    print(a)
+
+# Below, I've coded the first line of an if statement. Code the second line. The second line tells Python to display the string "OK". Don't forget to indent the second line.
+current_address = "1234"
+previous_address = "Bakerslane"
+
+if current_address == previous_address:
+    print("OK")
+
+# On line 1, test whether a has the same value as b. On line 2, write that c equals d. Don't forget to indent the second line.
+a = 1
+b = 2
+c = 3
+d = 4
+
+if a == b:
+    c == d
+
+# If total has the value of 100, tax has the value of 2. Code both lines. Don't forget to indent the second line.
+
+total = 100
+
+if total == 100:
+    tax = 2
+
+# If first_name is "Sherlock" last_name is "Holmes" and pal is "Watson". Code all three lines.
+first_name = "Sherlock"
+
+if first_name == "Sherlock":
+    last_name = "Holmes"
+    pal = "Watson"
+
+print(first_name)
+
+# If you_won is "yes" tell Python to display "congrats". Code both lines.
+
+you_won = "yes"
+
+if you_won == "yes":
+    print("congrats")
+
+species = "human"
+
+if species == "human":
+  print("correct")
+
+# Test if 2 equals 2. If it does, display ok
+
+if 2 == 2:
+    print("ok")
